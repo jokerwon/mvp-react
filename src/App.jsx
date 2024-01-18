36fac173ea50b34494dfb3f6df501438d6834340
JokerWon <1595672103@qq.com>
2024-01-18 14:26:37 +0800

    feat: update

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,22 @@ function Counter(props) {
   return <div>counter: {props.number}</div>
 }
 
-function Wrap() {
-  return <Counter number={10} />
-}
+let count = 10
 
 function App() {
   return (
     <div>
-      Hello React
-      <Counter number={10} />
-      <Counter number={100} />
-      <button onClick={() => console.log('clicked')}>Click</button>
+      {/* <Counter number={count} /> */}
+      count: {count}
+      <button
+        onClick={() => {
+          console.log('clicked')
+          count += 1
+          React.update()
+        }}
+      >
+        Click
+      </button>
     </div>
   )
 }
